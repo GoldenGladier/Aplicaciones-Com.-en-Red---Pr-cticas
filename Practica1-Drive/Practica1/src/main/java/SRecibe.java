55a263d5d4a94c27c6c5430b5a140cf8873c38b6
Omar Aguirre <omar.fi.wwr@gmail.com>
2022-03-08 01:00:34 -0600

    Se agrego funcion de envio de carpeta/directorio

--- Practica1-Drive/Practica1/src/main/java/SRecibe.java
+++ Practica1-Drive/Practica1/src/main/java/SRecibe.java
@@ -8,6 +8,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import javax.swing.JFileChooser;
 
 /*
@@ -38,27 +40,57 @@ public class SRecibe {
           f2.mkdirs();
           f2.setWritable(true);
           
+          // ------ Variables de apoyo para las diferentes acciones ------
+          long size;
+          String name;
+          String directoryName;
+          int bandera;
+          
           for(;;){
               Socket server = s.accept();
               System.out.println("Cliente conectado desde "+server.getInetAddress()+":"+server.getPort());
               DataInputStream dis = new DataInputStream(server.getInputStream());
               
-              int bandera = dis.readInt();
+              bandera = dis.readInt();
               System.out.println("Option: " + bandera);
-              String name = dis.readUTF();
-              long size = dis.readLong();
                       
               switch(bandera){
                   case 1:
                       // Ejecutar funcion #1
+                      size = dis.readLong();
+                      System.out.println("Tamaño de archivo: " + size);
+                      name = dis.readUTF();
+                      System.out.println("Nombre de archivo: " + name);                      
                       recibeArchivo(size, ruta_archivos + name, dis);
                       break;
                   case 2:
                       // Ejecutar funcion #2
+                      size = dis.readLong();
+                      name = dis.readUTF();
+                      System.out.println("Tamaño de archivo: " + size);                      
                       mandaArchivo(size, name, dis);
                       break;
-                  case 3:
+                  case 3: // Recibir zip
                       // Ejecutar funcion #3
+                      size = dis.readLong();
+                      System.out.println("Tamaño de archivo: " + size);
+                      name = dis.readUTF();
+                      System.out.println("Nombre de archivo: " + name);                      
+                      recibeArchivo(size, ruta_archivos + name, dis);
+                      UnzipFile(ruta_archivos + name, ruta_archivos);
+                      break;
+                  case 10: // Crear directorio (para uso interno del sistema)
+                      // Ejecutar funcion #10
+                      directoryName = dis.readUTF();
+                      System.out.println("Creando directorio " + directoryName);
+                        File directorio = new File(ruta_archivos + "/" + directoryName);
+                        if (!directorio.exists()) {
+                            if (directorio.mkdirs()) {
+                                System.out.println("Directorio creado");
+                            } else {
+                                System.out.println("Error al crear directorio");
+                            }
+                        }                      
                       break;
               }
 //              String nombre = dis.readUTF();
@@ -147,6 +179,56 @@ public class SRecibe {
         dis.close();        
     }
 
+    public static void UnzipFile(String ZipPath, String destPath) throws FileNotFoundException, IOException {        
+        File destDir = new File(destPath);
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(ZipPath));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+             File newFile = newFile(destDir, zipEntry);
+             if (zipEntry.isDirectory()) {
+                 if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                     throw new IOException("Failed to create directory " + newFile);
+                 }
+             } else {
+                 // fix for Windows-created archives
+                 File parent = newFile.getParentFile();
+                 if (!parent.isDirectory() && !parent.mkdirs()) {
+                     throw new IOException("Failed to create directory " + parent);
+                 }
+
+                 // write file content
+                 FileOutputStream fos = new FileOutputStream(newFile);
+                 int len;
+                 while ((len = zis.read(buffer)) > 0) {
+                     fos.write(buffer, 0, len);
+                 }
+                 fos.close();
+             }
+         zipEntry = zis.getNextEntry();
+        }           
+        zis.closeEntry();
+        zis.close();
+        System.out.println(destDir.getName() + " descomprimido...");
+        File fichero = new File(ZipPath);
+        if (fichero.delete())
+           System.out.println("El fichero ha sido borrado satisfactoriamente");
+        else
+           System.out.println("El fichero no puede ser borrado");        
+            }    
+    
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }    
     
     private void menu(){
         System.out.println("1. Subir archivo");
